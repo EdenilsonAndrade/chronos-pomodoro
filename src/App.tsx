210e2598066f9722217eec7712e7b1ae48dca93c
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-05-14 12:50:08 -0300

    chore: update content of <p> tag

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export function App() {
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Id praesentium
         illum pariatur corrupti iste est soluta ut, nam officiis sint, odit quos
-        nihil at, aut obcaecati sunt quas?
+        nihil at, aut obcaecati sunt quas? Dolore, sapiente.
       </p>
     </>
   );
