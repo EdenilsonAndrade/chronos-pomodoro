ae49406dbb2204a194fe70473cffa669cc3fab31
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-05-14 20:34:30 -0300

    feat: update heading styles and change prop type

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Olá mundo!</Heading>
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Id praesentium
         illum pariatur corrupti iste est soluta ut, nam officiis sint, odit quos
