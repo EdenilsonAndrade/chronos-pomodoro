1b74fdafd70a6705a427bd5ff1f5acad7a325de1
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-05-14 20:49:36 -0300

    feat: create tab structure for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id praesentium
-        illum pariatur corrupti iste est soluta ut, nam officiis sint, odit quos
-        nihil at, aut obcaecati sunt quas? Dolore, sapiente.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
