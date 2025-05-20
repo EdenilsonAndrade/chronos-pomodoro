445b3f2bae05e74ea6783172b12e1305ccce769c
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-05-20 04:22:09 -0300

    style: add CSS to DefaultInput component

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,13 +1,20 @@
+import styles from './styles.module.css';
+
 type DefaultInputProps = {
   id: string;
   labelText: string;
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, labelText, type, ...rest }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  labelText,
+  type,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
       <label htmlFor={id}>{labelText}</label>
-      <input type={type} id={id} {...rest} />
+      <input className={styles.input} type={type} id={id} {...rest} />
     </>
   );
 }
