124b635a2edb101978b08dc2fb3b21840579880f
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-05-21 05:05:02 -0300

    feat: add theme switch routine

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,52 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type IThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<IThemes>('dark');
+
+  function handleTheme(event: React.MouseEvent) {
+    event.preventDefault();
+    setTheme((prevTheme: IThemes) => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a className={styles.menuLink} href='#' aria-label='Home' title='Home'>
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Histórico'
+        title='Hitórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Tema'
+        title='Tema'
+        onClick={handleTheme}
+      >
         <SunIcon />
       </a>
     </nav>
