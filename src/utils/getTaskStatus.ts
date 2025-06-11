e419da31a523a3ef337ed32c897181c20761c2c3
EdenilsonAndrade <edenilsonbass@hotmail.com>
2025-06-11 04:12:46 -0300

    feat: add dictionary to display task types in table

--- src/utils/getTaskStatus.ts
+++ src/utils/getTaskStatus.ts
@@ -4,5 +4,5 @@ export function getTaskStatus(task: TaskModel, activeTask: TaskModel | null) {
   if (task.completeDate) return 'Completa';
   if (task.interruptDate) return 'Interrompida';
   if (task.id === activeTask?.id) return 'Em progresso';
-  return 'Em progresso';
+  return 'Abandonada';
 }
